02fe9cca0607f75dc7007fae8aaf33bd40cc0462
sayantan1821 <ksayantan1823@gmail.com>
2023-09-04 22:17:39 +0530

    add access all chat API

--- routes/api/chat.js
+++ routes/api/chat.js
@@ -5,6 +5,26 @@ const auth = require('../../middleware/auth');
 const Chat = require('../../models/Chat');
 const User = require('../../models/User');
 
+router.get('/', auth, async (req, res) => {
+    try {
+      Chat.find({ users: { $elemMatch: { $eq: req.user.id } } })
+        .populate("users", "-password")
+        .populate("groupAdmin", "-password")
+        .populate("latestMessage")
+        .sort({ updatedAt: -1 })
+        .then(async (results) => {
+          results = await User.populate(results, {
+            path: "latestMessage.sender",
+            select: "name pic email",
+          });
+          res.status(200).send(results);
+        });
+    } catch (error) {
+      res.status(400);
+      throw new Error(error.message);
+    }
+  });
+
 router.post('/createPersonalChat', auth, async (req, res) => {
   const { userId } = req.body;
   if (!userId) {
